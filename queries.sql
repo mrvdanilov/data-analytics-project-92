c4662821fe52dd375b5c08a9b9fccd3a1a7ec19d
Вугар Дамиров <mr_vdanilov95@mail.ru>
2024-05-09 21:05:34 +0300

    2

--- queries.sql
+++ queries.sql
@@ -26,10 +26,12 @@ with a as (
 b as (select avg(income) as avg_total from a
 ),
 
-c as (select
-    seller,
-    avg(income) as avg_sale
-from a group by 1)
+c as (
+    select
+        seller,
+        avg(income) as avg_sale
+    from a group by 1
+)
 
 select
     seller,
@@ -63,15 +65,15 @@ with a as (
 
 select
     age_category,
-    COUNT(*) as age_count
+    count(*) as age_count
 from a
 group by 1
 order by 1;
 
 select
-    TO_CHAR(sale_date, 'YYYY-MM') as selling_month,
-    COUNT(distinct customer_id) as total_customers,
-    FLOOR(SUM(s.quantity * p.price)) as income
+    to_char(sale_date, 'YYYY-MM') as selling_month,
+    count(distinct customer_id) as total_customers,
+    floor(sum(s.quantity * p.price)) as income
 from public.sales as s
 inner join employees as e
     on s.sales_person_id = e.employee_id
@@ -80,20 +82,20 @@ inner join products as p
 group by 1
 order by 1;
 
-SELECT DISTINCT ON (c.customer_id)
+select distinct on (c.customer_id)
     s.sale_date,
-    CONCAT(c.first_name, ' ', c.last_name) AS customer,
-    CONCAT(e.first_name, ' ', e.last_name) AS seller
-FROM
-    sales AS s
-INNER JOIN
-    customers AS c
-    ON s.customer_id = c.customer_id
-INNER JOIN
-    employees AS e
-    ON s.sales_person_id = e.employee_id
-INNER JOIN
-    products AS p
-    ON s.product_id = p.product_id
-WHERE p.price = 0
-ORDER BY c.customer_id, s.sale_date;
+    concat(c.first_name, ' ', c.last_name) as customer,
+    concat(e.first_name, ' ', e.last_name) as seller
+from
+    sales as s
+inner join
+    customers as c
+    on s.customer_id = c.customer_id
+inner join
+    employees as e
+    on s.sales_person_id = e.employee_id
+inner join
+    products as p
+    on s.product_id = p.product_id
+where p.price = 0
+order by c.customer_id, s.sale_date;
