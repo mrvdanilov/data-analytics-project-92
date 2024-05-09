b7235ec3f505a79ba24f6a2ec71222c39e75ac15
Вугар Дамиров <mr_vdanilov95@mail.ru>
2024-05-09 20:55:43 +0300

    1

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 ### Hexlet tests and linter status:
-[![Actions Status](https://github.com/mrvdanilov/data-analytics-project-92/workflows/hexlet-check/badge.svg)](https://github.com/mrvdanilov/data-analytics-project-92/actions)
+[![Actions Status](https://github.com/Vazlevski/data-analytics-project-92/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Vazlevski/data-analytics-project-92/actions)
--- queries.sql
+++ queries.sql
@@ -1,112 +1,99 @@
--- Посчитать количество покупателей
+select
+    concat(e.first_name, ' ', e.last_name) as seller,
+    count(s.sales_id) as operations,
+    floor(sum(s.quantity * p.price)) as income
+from public.sales as s
+inner join employees as e
+    on s.sales_person_id = e.employee_id
+inner join products as p
+    on s.product_id = p.product_id
+group by 1
+order by income desc
+limit 10;
 
-SELECT count(*) AS customers_count FROM customers;
+with a as (
+    select
+        concat(e.first_name, ' ', e.last_name) as seller,
+        avg(s.quantity * p.price) as income
+    from public.sales as s
+    inner join employees as e
+        on s.sales_person_id = e.employee_id
+    inner join products as p
+        on s.product_id = p.product_id
+    group by 1
+),
 
--- Посчитать записи и подписать категории возврастов
+b as (select avg(income) as avg_total from a
+),
 
-SELECT age_category, COUNT(*)
-FROM (
-    SELECT
-        CASE
-            WHEN age <= 15 THEN '10-15'
-            WHEN age BETWEEN 16 AND 25 THEN '16-25'
-            WHEN age BETWEEN 26 AND 40 THEN '26-40'
-            ELSE '40+'
-        END AS age_category,
-        age
-    FROM customers
-) t
-GROUP BY age_category
-ORDER BY age_category
+c as (select
+    seller,
+    avg(income) as avg_sale
+from a group by 1)
 
--- топ 10 продавцов по продажам
+select
+    seller,
+    floor(avg_sale) as average_income
+from c
+cross join b
+where avg_sale < avg_total
+order by average_income asc;
 
-SELECT
-   CONCAT(e.first_name, ' ', e.last_name) AS name,
-   COUNT(*) as operations,
-   SUM(p.price * s.quantity) AS income
-FROM
-   public.sales AS s
-   JOIN
-      public.products AS p
-      on s.product_id = p.product_id
-   JOIN
-      public.employees AS e
-      ON s.sales_person_id = e.employee_id
-GROUP BY
-   e.first_name,
-   e.last_name
-ORDER BY
-   income DESC LIMIT 10;
+select
+    concat(e.first_name, ' ', e.last_name) as seller,
+    to_char(sale_date, 'day') as day_of_week,
+    floor(sum(s.quantity * p.price)) as income
+from public.sales as s
+inner join employees as e
+    on s.sales_person_id = e.employee_id
+inner join products as p
+    on s.product_id = p.product_id
+group by 1, 2
+order by extract(isodow from max(s.sale_date)), 1;
 
--- Продавцы, чей средний доход за сделку меньше среднего дохода за сделку по всем продовцам
-
-WITH avg_income_per_sale AS (
-   SELECT ROUND(AVG(p.price * s.quantity)) AS average_income_per_sale
-   FROM public.sales AS s
-   JOIN public.products AS p
-   ON s.product_id = p.product_id
+with a as (
+    select
+        case
+            when age between 16 and 25 then '16-25'
+            when age between 26 and 40 then '26-40'
+            else '40+'
+        end as age_category
+    from customers
 )
-SELECT
-   CONCAT(e.first_name, ' ', e.last_name) AS name,
-   ROUND(AVG(p.price * s.quantity)) AS average_income_per_sale
+
+select
+    age_category,
+    COUNT(*) as age_count
+from a
+group by 1
+order by 1;
+
+select
+    TO_CHAR(sale_date, 'YYYY-MM') as selling_month,
+    COUNT(distinct customer_id) as total_customers,
+    FLOOR(SUM(s.quantity * p.price)) as income
+from public.sales as s
+inner join employees as e
+    on s.sales_person_id = e.employee_id
+inner join products as p
+    on s.product_id = p.product_id
+group by 1
+order by 1;
+
+SELECT DISTINCT ON (c.customer_id)
+    s.sale_date,
+    CONCAT(c.first_name, ' ', c.last_name) AS customer,
+    CONCAT(e.first_name, ' ', e.last_name) AS seller
 FROM
-   public.sales AS s
-   JOIN public.products AS p
-   ON s.product_id = p.product_id
-   JOIN public.employees AS e
-   ON s.sales_person_id = e.employee_id
-GROUP BY e.first_name, e.last_name
-HAVING AVG(p.price * s.quantity) < (SELECT average_income_per_sale FROM avg_income_per_sale)
-ORDER BY average_income_per_sale ASC;
-
--- Продажи отдельных продавцов по дням недели
-
-SELECT
-   CONCAT(e.first_name, ' ', e.last_name) AS name,
-   to_char(s.sale_date, 'day') AS weekday,
-   ROUND(SUM(s.quantity * p.price)) AS income
-from public.sales AS s
-join employees AS e ON s.sales_person_id = e.employee_id
-JOIN products AS p ON s.product_id = p.product_id
-GROUP BY CONCAT(e.first_name, ' ', e.last_name), weekday, to_char(s.sale_date, 'ID')
-ORDER BY to_char(s.sale_date, 'ID'), CONCAT(e.first_name, ' ', e.last_name)
-
--- Количество покупателей по месяцам и принесенная выручка
-
-SELECT
-   to_char(s.sale_date, 'YYYY-MM') AS date,
-   COUNT(DISTINCT s.customer_id) AS total_customers,
-   SUM(s.quantity * p.price) AS income
-FROM
-   public.sales AS s
-   JOIN
-      public.products AS p
-      ON s.product_id = p.product_id
-GROUP BY
-   date
-ORDER BY
-   date ASC;
-
--- Покупатели, первая покупка которых пришлась на акционный товар
-
-SELECT DISTINCT
-   ON(s.customer_id) CONCAT(c.first_name, ' ', c.last_name) AS customer,
-   s.sale_date,
-   CONCAT(e.first_name, ' ', e.last_name) AS seller
-FROM
-   public.sales AS s
-   JOIN
-      public.customers AS c
-      ON s.customer_id = c.customer_id
-   JOIN
-      public.products AS p
-      ON s.product_id = p.product_id
-   JOIN
-      public.employees AS e
-      ON s.sales_person_id = e.employee_id
-WHERE
-   p.price = 0
-ORDER BY
-   s.customer_id,
-   sale_date
+    sales AS s
+INNER JOIN
+    customers AS c
+    ON s.customer_id = c.customer_id
+INNER JOIN
+    employees AS e
+    ON s.sales_person_id = e.employee_id
+INNER JOIN
+    products AS p
+    ON s.product_id = p.product_id
+WHERE p.price = 0
+ORDER BY c.customer_id, s.sale_date;
