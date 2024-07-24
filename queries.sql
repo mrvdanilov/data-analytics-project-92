18367a055b6c6a7dba893b12a78973b746fd74cc
mrvdanilov <81223621+mrvdanilov@users.noreply.github.com>
2024-07-24 22:37:58 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,24 +1,6 @@
---  1
-/*
-Используем агрегирующую функцию COUNT() для расчета
-числа покупателей. Дополнительно прописал DISTINCT
-на случай, если в справочнике будут дублирующие значения,
-но результат для COUNT() и COUNT(DISTINCT) в данном случае
-не отличается
-*/
-
 SELECT COUNT(DISTINCT customer_id) AS "customers_count"
-FROM customers
+FROM customers;
 
---  2
-/*
-Джоиним два необходимых справочника employees и products по ключам
-employee_id и product_id соответственно на "таблицу фактов" sales, после чего
-создаем три поля: конкатинированное поле с именем и фамилией, число операций
-и сумму выручки (округляем значения до целых через FLOOR()) как произведение
-цены конкретного продукта на его количество. Группируем информацию по
-продавцам через GROUP BY, отображаем по убыванию выручки.
-*/
 WITH sellers AS (
     SELECT
         e.employee_id AS seller_id,
@@ -34,15 +16,6 @@ INNER JOIN sales AS s ON sellers.seller_id = s.sales_person_id
 INNER JOIN products AS p ON s.product_id = p.product_id
 GROUP BY 1 ORDER BY 3 DESC
 LIMIT 10;
-
--- 3
-/*
-Аналогично предыдущему запросу делаем джоины, но на этот раз делаем агрегацию
-по средней выручке продавца. Пишем "условие" того, что средняя выручка
-конкретного продавца меньше средней выручки по всем продавцам через подзапрос
-в HAVING, так как "работаем" с агрегированной функцией AVG().
-Округляем значения до целых через FLOOR().
-*/
 
 SELECT
     CONCAT(e.first_name, ' ', e.last_name) AS seller,
@@ -60,17 +33,7 @@ HAVING
         LEFT JOIN products AS p
             ON s.product_id = p.product_id
     )
-ORDER BY average_income ASC
-
---4
-/*
-Аналогично собираем таблицу из справочников и таблицы-факта, конвертируем дату в
-имена дней недели, группируем по имени продавца и дню недели, сортируем
-по тем же полям в прямом порядке.
-UPD: сортировка была некорректна, т.к. приводила к сортировке
-в алфавитном порядке. Изменено на to_char(s.sale_date, 'ID')
-- конвертация даты в дни недели в ISO-формате.
-*/
+ORDER BY average_income ASC;
 
 SELECT
 	CONCAT(e.first_name,' ', e.last_name) AS seller,
@@ -82,15 +45,7 @@ ON e.employee_id = s.sales_person_id
 	LEFT JOIN products p
 ON s.product_id = p.product_id
 GROUP BY seller, day_of_week, to_char(s.sale_date, 'ID')
-ORDER BY to_char(s.sale_date, 'ID'), seller
-
---5
-/*
-Через CASE прописываем условие для дифференциации покупателей по возрастным
-группам (с учетом того, что BETWEEN включает в себя оба "края"),
-считаем количество строк при группироке по возрастной
-группе.
-*/
+ORDER BY to_char(s.sale_date, 'ID'), seller;
 
 SELECT
     CASE
@@ -101,16 +56,7 @@ SELECT
     COUNT(*) AS age_count
 FROM customers
 GROUP BY age_category
-ORDER BY age_category
-
---6
-/*
-Через джоины собираем таблицу из таблицы фактов и двух справочников,
-а после SELECT'ом выбираем три поля: 1) дату в формате YYYY-MM,
-2) число уникальных покупателей через COUNT(DISTINCT),
-3) выручка как цена за единицу товара на количество проданных штук
-с округлением до целых.
-*/
+ORDER BY age_category;
 
 SELECT
     TO_CHAR(s.sale_date, 'YYYY-MM') AS selling_month,
@@ -122,13 +68,7 @@ LEFT JOIN customers AS c
 LEFT JOIN products AS p
     ON s.product_id = p.product_id
 GROUP BY selling_month
-ORDER BY selling_month ASC
-
---7
-/*
-Оконными функциями ищем "первые вхождения" промоционных товаров и соотв. дату.
-Обертка в CTE для удобства написания запроса :)
-*/
+ORDER BY selling_month ASC;
 
 WITH initial_table AS (
     SELECT
@@ -150,7 +90,6 @@ WITH initial_table AS (
     LEFT JOIN products AS p
         ON s.product_id = p.product_id
 ),
-
 first_val_disc_table AS (
     SELECT DISTINCT ON (customer)
         customer,
@@ -159,5 +98,5 @@ first_val_disc_table AS (
     FROM initial_table
     WHERE first_val_disc = 0
 )
-
 SELECT * FROM first_val_disc_table
+;
